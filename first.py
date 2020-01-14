6db44e8db557beda6bc2f1cda6f520d4bda9135d
Duan <chengguiduan@gmail.com>
2020-01-14 22:35:28 +0800

    first commit

---
+++ first.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 21:41:38 2019
+
+@author: Duan
+"""
+
+def get_gender (sex):
+    if sex is "m":
+        sex = "Male"
+    elif sex is "f":
+        sex = "female"
+    print (sex)
+        
+    
+get_gender("f")
+get_gender("m")
+get_gender()    
